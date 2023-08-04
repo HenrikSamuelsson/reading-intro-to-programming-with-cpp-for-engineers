90d3fd9ef6bfd494346254366ca2d7d3ba3c011a
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-08-04 07:14:38 +0200

    Bug fix exercise 2.3.

--- chapter-02/exercise-03/ipcppe-ch02-ex03-main.cpp
+++ chapter-02/exercise-03/ipcppe-ch02-ex03-main.cpp
@@ -6,19 +6,26 @@ int main () {
     double interval_start{1}; 
     double interval_end{2};
     double interval_mid{};
-    double interval_tolerance{0.001};
+    double interval_tolerance{0.000001};
 
-    while (interval_end - interval_start > interval_tolerance) {
-        interval_mid = (interval_end - interval_start) / 2;
+    bool found_a_root{false};
+    while (found_a_root == false) {
+        interval_mid = (interval_end + interval_start) / 2.0;
+        
         if ((f(interval_mid) < 0) == (f(interval_end) < 0)) {
             interval_end = interval_mid;
         }
         else {
             interval_start = interval_mid;
         }
+
+        if (f(interval_mid) < interval_tolerance &&
+        -f(interval_mid) < interval_tolerance) {
+            found_a_root = true;
+        }
     }
 
-    std::cout << interval_mid << std::endl;
+    std::cout << "One root is "  << interval_mid << std::endl;
 }
 
 double f(double x) {
