13bc40a7f79afd0465091d8440db459613ab762e
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2023-08-12 09:08:51 +0200

    Add initial solution to exercise 3.3.

---
+++ chapter-03/exercise-01/ipcppe-ch03-ex03-main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+double fahrenheitToCelsius(double temperatureInFahrenheit);
+
+int main()
+{
+    std::cout << "Temperature Converter Fahrenheit to Celsius" << std::endl;
+    std::cout << "Enter temperature in Celsius: ";
+
+    double temperatureInFahrenheit{};
+    std::cin >> temperatureInFahrenheit;
+
+    double temperatureInCelsius{};
+    temperatureInCelsius = fahrenheitToCelsius(temperatureInFahrenheit);
+
+    std::cout << temperatureInFahrenheit << " degrees Fahrenheit equals "
+              << temperatureInCelsius << " degrees Celsius." 
+              << std::endl;
+
+    return 0;
+}
+
+double fahrenheitToCelsius(double temperatureInFahrenheit)
+{
+    return (temperatureInFahrenheit - 32.0) * 5.0 / 9.0;
+}
