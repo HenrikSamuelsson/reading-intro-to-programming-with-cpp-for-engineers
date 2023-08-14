ae65750278b1dfc93cb35ed09da34041a0ccc6c4
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2023-08-14 06:29:11 +0200

    Autoformat the code.

--- chapter-02/exercise-05/ipcppe-ch02-ex05-main.cpp
+++ chapter-02/exercise-05/ipcppe-ch02-ex05-main.cpp
@@ -5,15 +5,9 @@
 // Published by Wiley, 2020
 //
 // The software is supplied as is and for educational purposes
-// without any guarantees nor responsibility of its use in any application. 
+// without any guarantees nor responsibility of its use in any application.
 //
 // ==========================================================================
-
-
-
-
-
-
 
 #include <iostream>
 #include <algorithm>
@@ -21,76 +15,61 @@
 #include <vector>
 #include <iterator>
 
-
-
 // Listing them here allows omitting std::
 using std::cout, std::cin, std::endl;
 using std::string, std::vector;
 
-
-
-
-
-void  letter_histogram( void )
+void letter_histogram(void)
 {
-	// Make slot for each letter, from 'a' to 'z'
-	// Initial count set to 0 for each.
-	std::vector histogram( 'z' - 'a' + 1, 0 );
+    // Make slot for each letter, from 'a' to 'z'
+    // Initial count set to 0 for each.
+    std::vector histogram('z' - 'a' + 1, 0);
 
-	std::string in_str;
+    std::string in_str;
 
-	//std::cin >> in_str;
-	in_str = "AGH University of Science and Technology";
+    // std::cin >> in_str;
+    in_str = "AGH University of Science and Technology";
 
-	for( auto c : in_str )
-		if( isalpha( c ) )
-			++ histogram[ tolower( c ) - 'a' ];
+    for (auto c : in_str)
+        if (isalpha(c))
+            ++histogram[tolower(c) - 'a'];
 
-	cout << endl;  
+    cout << endl;
 
-	for( auto k { 'a' }; k <= 'z'; ++ k )
-		cout << k << " ";		// Print all characters �a� to �z�
+    for (auto k{'a'}; k <= 'z'; ++k)
+        cout << k << " "; // Print all characters �a� to �z�
 
-	cout << endl;  
+    cout << endl;
 
-	for( auto h : histogram )
-		cout << h << " ";		// Print histogram values
+    for (auto h : histogram)
+        cout << h << " "; // Print histogram values
 
-	cout << endl;  
+    cout << endl;
 }
-
-
-
 
 // 2nd version using the ST algorithms
-void letter_histogram_stl( void )
+void letter_histogram_stl(void)
 {
-	std::vector histogram( 'z' - 'a' + 1, 0 );
+    std::vector histogram('z' - 'a' + 1, 0);
 
-	std::string in_str;
+    std::string in_str;
 
-	//std::cin >> in_str;
-	in_str = "AGH University of Science and Technology";
+    // std::cin >> in_str;
+    in_str = "AGH University of Science and Technology";
 
+    std::for_each(in_str.begin(), in_str.end(),
+                  [&histogram](const auto c)
+                  {
+                      if (isalpha(c))
+                          ++histogram[tolower(c) - 'a'];
+                  });
 
-	std::for_each(	in_str.begin(), in_str.end(), 
-		[ & histogram ] ( const auto c )	{ 
-		if( isalpha( c ) ) 
-			++ histogram[ tolower( c ) - 'a' ];
-	} 
-	);
+    cout << endl;
+    std::generate_n(std::ostream_iterator<char>(std::cout, " "), histogram.size(), [k = 'a']() mutable
+                    { return k++; });
 
+    cout << endl;
+    std::copy(histogram.begin(), histogram.end(), std::ostream_iterator<int>(std::cout, " "));
 
-	cout << endl;  
-	std::generate_n( std::ostream_iterator< char >( std::cout, " " ), histogram.size(), [ k = 'a' ] () mutable { return k ++; } );
-
-	cout << endl;  
-	std::copy( histogram.begin(), histogram.end(), std::ostream_iterator< int >( std::cout, " " ) );
-
-	cout << endl;  
-
+    cout << endl;
 }
-
-
-
-
